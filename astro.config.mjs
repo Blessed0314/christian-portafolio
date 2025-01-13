2c4526d0048d2bf77d02509e56871488d2c48c6f
blessed0314 <christian.daniel999@gmail.com>
2025-01-13 15:22:35 -0500

    Actualización portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/christian-portfolio/',
+    base: '/christian-portafolio/docs/',
     outDir: './docs',
 });
